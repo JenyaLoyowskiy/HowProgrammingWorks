ac1b2fa996cf837f934ec9d746324d3d0a06dbf6
JenyaLoyowskiy <47608994+JenyaLoyowskiy@users.noreply.github.com>
2019-09-09 17:19:23 +0300

    Update homework.js

--- HW-1/homework.js
+++ HW-1/homework.js
@@ -1,97 +1,97 @@
-        //ИДЕНТИФИКАТОРЫ
-      let name = 'Jenya';
-      const year = 2002;
-      let printName = name => {
-        console.log(`Hello ${name}!`);
-      }
-      printName('Jenya');
-      printName(name);
-      printName(year);
-      
-        //ЦИКЛЫ
-      for (let i = 15; i <= 30; i++) {
-        if (i % 2 !== 0) {
-          console.log(i);
-        }
-      }
-      
-      let range = (start, end) => {
-        for (start; start < end; start++) {
-          if (start % 2 !== 0) {
-            console.log(start);
-          }
-        }
-      }
-      range(15,30)
-      
-      
-        //ФУНКЦИИ
-      let average = (a,b) => {
-        console.log(a+b);
-      }
-      
-      let square = x => {
-        return Math.pow(x, 2);
-      }
-      
-      let cube = x => {
-        return Math.pow(x, 3);
-      };
-      
-      for (let i = 0; i < 3; i++) {
-        average(square(i), cube(i));
-      }
-      
-        //ОБЪЕКТЫ
-      const first_obj = {
-        name: 'Jenya'
-      }
-      let second_obj = {
-        name: 'Jenya'
-      }
-      first_obj.name = 'Vova';
-      second_obj.name = 'Vova';
-      
-      console.log(first_obj.name, second_obj.name);
-      
-      // first_obj = {
-      //   name: 'Egor'
-      // }
-      // second_obj = {
-      //   name: 'Egor'
-      // }
-      console.log(first_obj.name, second_obj.name);
-      
-      let createUser = (name, phone) => {
-        console.log({name: name, phone: phone});
-      }
-      
-      createUser('Marcus Aurelius','+380445554433');
-      
-       //МАССИВЫ
-      let people = [{name: 'Jack', phone: '+380445554433'},{name: 'Bob', phone: '+380957248835'},{name: 'Rick', phone: '+483480234866'}];
-      
-      let findPhoneByName = name => {
-        for (let i = 0; i < people.length; i++) {
-          if (people[i].name == name) {
-            console.log(people[i].phone);
-          }
-        }
-      }
-      findPhoneByName('Rick');
-      
-       //КОЛЛЕКЦИИ ХЕШ-ТАБЛИЦЫ
-      let object = {
-        "Jack": "+380445554433",
-        "Bob": "+380957248835",
-        "Rick": "+483480234866"
-      }
-      
-      let findPhoneByName = name => {
-        for ( const n in object) {
-          if (n == name) {
-            console.log(object[n])
-          }
-        }
-      }
-      findPhoneByName('Bob');
+    //ИДЕНТИФИКАТОРЫ
+let name = 'Jenya';
+const year = 2002;
+let printName = name => {
+  console.log(`Hello ${name}!`);
+}
+printName('Jenya');
+printName(name);
+printName(year);
+
+    //ЦИКЛЫ
+for (let i = 15; i <= 30; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+let range = (start, end) => {
+  for (start; start < end; start++) {
+    if (start % 2 !== 0) {
+      console.log(start);
+    }
+  }
+}
+range(15,30)
+
+
+    //ФУНКЦИИ
+let average = (a,b) => {
+  console.log(a+b);
+}
+
+let square = x => {
+  return Math.pow(x, 2);
+}
+
+let cube = x => {
+  return Math.pow(x, 3);
+};
+
+for (let i = 0; i < 3; i++) {
+  average(square(i), cube(i));
+}
+
+    //ОБЪЕКТЫ
+const first_obj = {
+  name: 'Jenya'
+}
+let second_obj = {
+  name: 'Jenya'
+}
+first_obj.name = 'Vova';
+second_obj.name = 'Vova';
+
+console.log(first_obj.name, second_obj.name);
+
+// first_obj = {
+//   name: 'Egor'
+// }
+// second_obj = {
+//   name: 'Egor'
+// }
+console.log(first_obj.name, second_obj.name);
+
+let createUser = (name, phone) => {
+  console.log({name: name, phone: phone});
+}
+
+createUser('Marcus Aurelius','+380445554433');
+
+    //МАССИВЫ
+let people = [{name: 'Jack', phone: '+380445554433'},{name: 'Bob', phone: '+380957248835'},{name: 'Rick', phone: '+483480234866'}];
+
+let findPhoneByName = name => {
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].name == name) {
+      console.log(people[i].phone);
+    }
+  }
+}
+findPhoneByName('Rick');
+
+    //КОЛЛЕКЦИИ ХЕШ-ТАБЛИЦЫ
+let object = {
+  "Jack": "+380445554433",
+  "Bob": "+380957248835",
+  "Rick": "+483480234866"
+}
+
+let findPhoneByName = name => {
+  for ( const n in object) {
+    if (n == name) {
+      console.log(object[n])
+    }
+  }
+}
+findPhoneByName('Bob');
